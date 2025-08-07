aa5b71e42ebb891dbfd07dbb54c57d23e4323382
Zatiel <44403468+callmezatiel@users.noreply.github.com>
2025-08-07 01:20:31 -0600

    Update protocol.sh

--- protocol.sh
+++ protocol.sh
@@ -10,7 +10,7 @@ RED="\033[0;31m"; GREEN="\033[0;32m"; YELLOW="\033[1;33m"; NC="\033[0m"
 [[ $EUID -ne 0 ]] && { echo -e "${RED}[!] Run as root.${NC}"; exit 1; }
 
 # Banner
-clear; figlet -c "Net Recon Pro" | lolcat
+clear; figlet -c "Protocol" | lolcat
 echo -e "${GREEN}Author: Zatiel | Red Team Toolkit${NC}\n"
 
 # Get system info
